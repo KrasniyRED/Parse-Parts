2b3340987e183eca8114a211bdb2a36f961973c1
Ivan Bedov <freginder@mail.ru>
2024-05-30 17:52:38 +0700

    Базовый класс комманды и ViewModel для главного окна

---
+++ Parse Parts/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Parse_Parts.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parse_Parts.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+
+    }
+}
