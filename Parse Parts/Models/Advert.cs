b696cb3f7d5dd0a6bc674226c58620ab48602b8c
Ivan Bedov <freginder@mail.ru>
2024-06-01 10:50:31 +0700

    Создание модели представления объявления

---
+++ Parse Parts/Models/Advert.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parse_Parts.Models
+{
+    internal class Advert
+    {
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string Price { get; set; }
+        public string Description { get; set; }
+        public string Photo {  get; set; }
+
+        public Advert(string title, string url, string price, string description, string photo)
+        {
+            Title = title;
+            Url = url;
+            Price = price;
+            Description = description;
+            Photo = photo;
+        }
+    }
+}
