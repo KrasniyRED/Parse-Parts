ec43668701bb949526dbb11bf48966b39c7177d9
Ivan Bedov <freginder@mail.ru>
2024-06-01 22:49:11 +0700

    Первая версия асинхронной команды

---
+++ Parse Parts/Infrastructure/Commands/MainCommandAsync.cs
@@ -0,0 +1,27 @@
+﻿using Parse_Parts.Infrastructure.Commands.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parse_Parts.Infrastructure.Commands
+{
+    internal class MainCommandAsync : CommandAsync
+    {
+        private readonly Func<Task> _Execute;
+        private readonly Func<object,bool> _CanExecute;
+
+        public MainCommandAsync(Func<Task> Execute, Func<object,bool> CanExecute)
+        {
+            _Execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            _CanExecute = CanExecute;
+        }
+        public override bool CanExecute(object parameter) => _CanExecute?.Invoke(parameter) ?? true;
+        public override Task ExecuteAsync(object parameter)
+        {
+            return _Execute();
+        }
+    }
+}
