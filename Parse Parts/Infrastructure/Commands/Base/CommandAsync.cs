1d5973f37450313110b852645e57acc3484c2dd1
Ivan Bedov <freginder@mail.ru>
2024-06-01 22:24:06 +0700

    Создание базовой асинхронной команды

---
+++ Parse Parts/Infrastructure/Commands/Base/CommandAsync.cs
@@ -0,0 +1,33 @@
+﻿using Parse_Parts.Infrastructure.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Parse_Parts.Infrastructure.Commands.Base
+{
+    internal abstract class CommandAsync : ICommandAsync
+    {
+        public abstract bool CanExecute(object parameter);
+        
+        public abstract Task ExecuteAsync(object parameter);
+        
+        public async void Execute(object parameter)
+        {
+            await ExecuteAsync(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        protected void RaiseCanExecuteChanged()
+        {
+            CommandManager.InvalidateRequerySuggested();
+        }
+    }
+}
